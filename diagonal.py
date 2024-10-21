a3efa17173776114d950a85a250dcefd16c82c46
Michael Bond <bondpapi@yahoo.com>
2024-10-21 12:47:48 +0300

    Commit changes

--- diagonal.py
+++ diagonal.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-# Set printoptions to use the legacy string formatting from NumPy 1.13  
-np.set_printoptions(legacy='1.13')
+# Set printoptions to use the legacy string formatting from NumPy 1.13
+np.set_printoptions(legacy="1.13")
 
 # Take input for N and M, and create a NumPy array of shape (N, M) with all zeros
 N, M = map(int, input().split())
